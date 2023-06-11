220423b550c197855ca8b2ef5185c028384ccfd6
Aliyanishere <hussainaliyan531@gmail.com>
2023-06-11 23:31:04 +0500

    node version fix

---
+++ build/precache-manifest.67dd6a7231f8440f49bc4e85964464b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa34255ac94bf60a56596b99ffe31d68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b89c40c100b15742614b",
+    "url": "/static/css/main.ca15c005.chunk.css"
+  },
+  {
+    "revision": "0eb70cead629abdd176c",
+    "url": "/static/js/2.18876ed5.chunk.js"
+  },
+  {
+    "revision": "7dc2529ea9c355027961799bc1ffe0c3",
+    "url": "/static/js/2.18876ed5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b89c40c100b15742614b",
+    "url": "/static/js/main.c4040cb0.chunk.js"
+  },
+  {
+    "revision": "949eb168f1d833ae83d5",
+    "url": "/static/js/runtime-main.743d20cc.js"
+  }
+]);
